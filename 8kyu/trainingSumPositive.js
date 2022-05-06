92dca51778c4490313eff349b2f63b041d30e656
Anthony Gonzales <anthonypatrickg@gmail.com>
2022-05-06 00:01:01 -0700

    dailyChallenge

---
+++ 8kyu/trainingSumPositive.js
@@ -0,0 +1,20 @@
+/* Instructions
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+// Solution
+function positiveSum(arr) {
+    //   if there is nothing just return sum
+      if (arr.length === 0){
+        return 0;
+      } else {
+        let sum = arr.filter(nums => nums >= 0);
+        return sum.reduce((a,b)=> a + b,0);
+      }
+}
+
+// Test Cases
+positiveSum([1,-2,3,4,5]);
+positiveSum([]);
